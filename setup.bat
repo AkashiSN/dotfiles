3bd1696c04d042855d1c4305bc6b889c26352b74
AkashiSN <btorntireinvynriy@gmail.com>
2021-01-04 23:54:38 +0900

    update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,7 +1,22 @@
+rem PowerShell Profiles
 md %homedrive%%homepath%\Documents\PowerShell
 md %homedrive%%homepath%\Documents\PowerShell\Scripts
+del %homedrive%%homepath%\Documents\PowerShell\Profile.ps1
+del %homedrive%%homepath%\Documents\PowerShell\Scripts\encode.py
 mklink %homedrive%%homepath%\Documents\PowerShell\Profile.ps1 %cd%\Profile.ps1
 mklink %homedrive%%homepath%\Documents\PowerShell\Scripts\encode.py %cd%\scripts\encode.py
 
-git config --global gpg.program 'C:/Program Files (x86)/gnupg/bin/gpg.exe'
-git config --global core.sshCommand 'C:/Windows/System32/OpenSSH/ssh.exe'
+rem ssh config
+md %homedrive%%homepath%\.ssh
+del %homedrive%%homepath%\.ssh\config
+del %homedrive%%homepath%\.ssh\gpg.pub
+mklink %homedrive%%homepath%\.ssh\config %cd%\.ssh\config
+mklink %homedrive%%homepath%\.ssh\gpg.pub %cd%\.ssh\gpg.pub
+
+rem git config
+del %homedrive%%homepath%\.gitconfig
+copy %cd%\.gitconfig %homedrive%%homepath%\.gitconfig
+git config --global gpg.program "C:/Program Files (x86)/GnuPG/bin/gpg.exe"
+git config --global core.sshCommand "C:/Windows/System32/OpenSSH/ssh.exe"
+
+pause
