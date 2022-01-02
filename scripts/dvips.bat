8d11e8aaade52f747413ecd7728fd06496017ea9
AkashiSN <btorntireinvynriy@gmail.com>
2022-01-02 13:08:29 +0900

    remove script

--- scripts/dvips.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-for /f "usebackq delims=" %%A in (`pwsh -noprofile -Command "%~dp0get_relative_path.ps1" "%*"`) do (
-  set ARGS=%%A
-)
-
-docker run --rm --name="dvips" -v "%CD%:/workdir" akashisn/latexmk:full dvips %ARGS%
--- scripts/latex.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-
-for /f "usebackq delims=" %%A in (`pwsh -noprofile -Command "%~dp0get_relative_path.ps1" "%*"`) do (
-  set ARGS=%%A
-)
-
-docker run --rm --name="latex" -v "%CD%:/workdir" akashisn/latexmk:full latex %ARGS%
