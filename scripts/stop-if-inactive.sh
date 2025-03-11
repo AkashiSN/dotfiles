e4a1ad382f74c99bab5c21452d365e95b0ae9952
Soichiro Nishi <su-nishi@kddi.com>
2025-03-11 08:47:48 +0000

    update script

--- scripts/stop-if-inactive.sh
+++ scripts/stop-if-inactive.sh
@@ -1,17 +1,23 @@
+#!/bin/bash
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-#!/bin/bash
 set -euo pipefail
-CONFIG=$(cat /home/ec2-user/.ec2/autoshutdown-configuration)
+# This script is invoked in /etc/cron.d/ec2-automatic-shutdown
+# which uses the full path to the file: /home/<username>/.ec2/stop-if-inactive.sh
+# Parse the username from the file path, which can vary based on the OS.
+USER=$(echo $0 | cut -d'/' -f3)
+CONFIG=$(cat /home/$USER/.ec2/autoshutdown-configuration)
 SHUTDOWN_TIMEOUT=${CONFIG#*=}
 if ! [[ $SHUTDOWN_TIMEOUT =~ ^[0-9]*$ ]]; then
     echo "shutdown timeout is invalid"
     exit 1
 fi
+
 is_shutting_down() {
     is_shutting_down_ubuntu &> /dev/null || is_shutting_down_al1 &> /dev/null || is_shutting_down_al2 &> /dev/null || is_shutting_down_al2023 &> /dev/null
 }
+
 is_shutting_down_ubuntu() {
     local TIMEOUT
     TIMEOUT=$(busctl get-property org.freedesktop.login1 /org/freedesktop/login1 org.freedesktop.login1.Manager ScheduledShutdown)
@@ -26,9 +32,11 @@ is_shutting_down_ubuntu() {
         return 0
     fi
 }
+
 is_shutting_down_al1() {
     pgrep shutdown
 }
+
 is_shutting_down_al2() {
     local FILE
     FILE=/run/systemd/shutdown/scheduled
@@ -38,6 +46,7 @@ is_shutting_down_al2() {
         return 1
     fi
 }
+
 is_shutting_down_al2023() {
     local TIMEOUT
     TIMEOUT=$(busctl get-property org.freedesktop.login1 /org/freedesktop/login1 org.freedesktop.login1.Manager ScheduledShutdown)
@@ -52,25 +61,21 @@ is_shutting_down_al2023() {
         return 0
     fi
 }
-is_vfs_connected() {
-    pgrep -f vfs-worker >/dev/null
-}
 
 is_vscode_connected() {
-    pgrep -u ec2-user -f .vscode-server/bin/ -a | grep -v -F 'shellIntegration-bash.sh' >/dev/null || \
-    pgrep -u ec2-user -f /home/ec2-user/.vscode-server/code- -a >/dev/null
+    pgrep -u $USER -f .vscode-server/bin/ -a | grep -v -F 'shellIntegration-bash.sh' >/dev/null
 }
 
 if is_shutting_down; then
-    if [[ ! $SHUTDOWN_TIMEOUT =~ ^[0-9]+$ ]] || is_vfs_connected || is_vscode_connected; then
+    if [[ ! $SHUTDOWN_TIMEOUT =~ ^[0-9]+$ ]] || is_vscode_connected; then
         sudo shutdown -c
-        echo > "/home/ec2-user/.ec2/autoshutdown-timestamp"
+        echo > "/home/$USER/.ec2/autoshutdown-timestamp"
     else
         TIMESTAMP=$(date +%s)
-        echo "$TIMESTAMP" > "/home/ec2-user/.ec2/autoshutdown-timestamp"
+        echo "$TIMESTAMP" > "/home/$USER/.ec2/autoshutdown-timestamp"
     fi
 else
-    if [[ $SHUTDOWN_TIMEOUT =~ ^[0-9]+$ ]] && ! is_vfs_connected && ! is_vscode_connected; then
+    if [[ $SHUTDOWN_TIMEOUT =~ ^[0-9]+$ ]] && ! is_vscode_connected; then
         sudo shutdown -h $SHUTDOWN_TIMEOUT
     fi
 fi
