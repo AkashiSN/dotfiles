aecb85a1d21a501e18478bc9b1cc57bb67e31858
AkashiSN <btorntireinvynriy@gmail.com>
2026-06-15 01:21:47 +0900

    Fix bufferline tab close collapsing split editor windows
    タブの × / 右クリックが既定の bdelete! を使っていたため、分割エディタで
    開いたファイルのタブを閉じるとそのペインごと閉じ(vanilla Vim 挙動)、
    畳み続けると nvim ごと終了していた。<leader>bd / SmartQ と同じ
    Snacks.bufdelete に統一し、ウィンドウを保ったままバッファだけ削除する。

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -28,6 +28,14 @@ return {
     opts = {
       options = {
         diagnostics = "nvim_lsp",
+        -- タブの × / 右クリックで閉じる時も Snacks.bufdelete を使う。
+        -- 既定の `bdelete! %d` は、分割エディタ(複数ペイン)で開いたファイルの
+        -- タブを閉じると、そのバッファを表示していたウィンドウごと閉じてしまう
+        -- (vanilla Vim の挙動)。ペインを次々畳んでいくと最終的に nvim ごと
+        -- 終了する事故になるため、ウィンドウを保ったままバッファだけ消す
+        -- Snacks.bufdelete に統一する(<leader>bd / SmartQ と同じ挙動)。
+        close_command = function(n) Snacks.bufdelete(n) end,
+        right_mouse_command = function(n) Snacks.bufdelete(n) end,
         offsets = {
           { filetype = "neo-tree", text = "Explorer", separator = true },
         },
